964ecd954d09a404a97d772cd3f0369c854593e6
91hongppie <91hongppie@gmail.com>
2019-08-20 20:02:38 +0900

    190820

---
+++ palindrome/세상의 모든 팰린드롬2.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(1, N+1):
+    count = 0
+    words = input()
+    for j in range(len(words)//2):
+        if words[j] == words[-1-j]:
+            count+=1
+        elif words[j] == '*' or words[-1-j] == '*':
+            count+=1
+        else:
+            print('#{} Not exist'.format(i))
+            break
+    if count == len(words)//2:
+        print('#{} Exist'.format(i))
