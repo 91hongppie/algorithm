c69b9238f4ba0cad699b5039edc102e5791f06f4
91hongppie <91hongppie@gmail.com>
2019-08-21 17:49:26 +0900

    190821 first

--- 보충/bomber1.py
+++ 보충/bomber1.py
@@ -7,13 +7,19 @@ for i in range(1, N+1):
     num = int(input())
     board = [list(map(int, input().split())) for _ in range(num)]
     result = 0
+
     for r in range(num):
-        row_sum = col_sum = 0
-        for c in range(num):
-            row_sum = sum(board[r])
-            col_sum += (board[r][c])
-        if result < row_sum + col_sum:
-            result = row_sum + col_sum
-            R = r
-            C = c
-    print(R, C, result)
+        row_sum = 0
+        row_sum = sum(board[r])
+        for s in range(num):
+            col_sum = 0
+            for c in range(num):
+                if c == r:
+                    pass
+                else:    
+                    col_sum += (board[c][s])
+            if result <= row_sum + col_sum:
+                result = row_sum + col_sum
+                R = r
+                C = s
+    print('#{} {} {} {}'.format(i, R, C, result))
