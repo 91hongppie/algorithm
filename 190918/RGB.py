a2773f936e52f08f64a0fc1f138b10ca730f6a36
91hongppie <91hongppie@gmail.com>
2019-09-18 21:42:21 +0900

    190918 home first

--- 190918/RGB.py
+++ 190918/RGB.py
@@ -1,14 +1,12 @@
 import sys
 sys.stdin = open('RGB.txt', 'r')
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**9)
 
 
 def DFS(r, c, col):
-    global result, visit
     dx = [0, 0, 1, -1]
     dy = [1, -1, 0, 0]
     visit[r][c] = True
-    visit1[r][c] = True
     for k in range(4):
         x1 = r + dx[k]
         y1 = c + dy[k]
@@ -17,6 +15,27 @@ def DFS(r, c, col):
                 if board[x1][y1] == col:
                     visit[x1][y1] = True
                     DFS(x1, y1, col)
+
+
+def DFS1(r, c, col):
+    global board
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+    visit1[r][c] = True
+    for k in range(4):
+        x1 = r + dx[k]
+        y1 = c + dy[k]
+        if 0 <= x1 < N and 0 <= y1 < N:
+            if visit1[x1][y1] == False:
+                if col == 'R' or col == 'G':
+                    if board[x1][y1] == 'R' or board[x1][y1] == 'G':
+                        visit1[x1][y1] = True
+                        DFS1(x1, y1, col)
+                else:
+                    if board[x1][y1] == col:
+                        visit1[x1][y1] = True
+                        DFS1(x1, y1, col)
+
 
 
 N = int(input())
@@ -32,4 +51,7 @@ for i in range(N):
         if not visit[i][j]:
             DFS(i, j, board[i][j])
             result += 1
-print(result)
+        if not visit1[i][j]:
+            DFS1(i, j, board[i][j])
+            result1 += 1
+print('{} {}'.format(result, result1))
