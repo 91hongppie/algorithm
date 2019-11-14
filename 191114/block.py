1a3835f2252fc14fcd208777d143fd32724c6a64
조규홍 <marioz9@naver.com>
2019-11-14 17:59:52 +0900

    191114 third

--- 191114/block.py
+++ 191114/block.py
@@ -1,56 +1,66 @@
 import sys
 sys.stdin = open('block.txt', 'r')
+import copy
+from pprint import pprint
+from collections import deque
 
-def DFS(row, col, block_map, nums):
-    global result
-    if nums == N:
-        count = 0
-        for g in range(H):
-            for k in range(W):
-                if block_map[g][k] != 0:
-                    count += 1
-        result = min(result, count)
+def DFS(r, c, block_map, nums):
+    if nums == times:
         return
-    crash(row, col, block_map)
-    for h in range(H-1, -1, -1):
-        for wi in range(W-1, -1, -1):
-            if block_map[h][wi] == 0:
-                for b in range(h, -1, -1):
-                    if block_map[b][wi] != 0:
-                        block_map[h][wi], block_map[b][wi] = block_map[b][wi], block_map[h][wi]
-    for i in range(H):
-        for j in range(W):
-            if board[i][j] != 0 and (i == 0 or board[i-1][j] == 0):
-                DFS(i, j, board, nums+1)
+    if 0 <= r < H and 0 <= c < W:
+        if block_map[r][c] == 1:
+            for u in range(len(top_list)):
+                if top_list[u] != -1:
+                    block_map[top_list[u]][u] = 0
+                    top_list[u] += 1
+                    DFS(top_list[u], u, block_map, nums+1)
+                    block_map[top_list[u]][u] = 1
+                    top_list[u] -= 1
 
-
-def crash(r, c, crash_map):
-    if crash_map[r][c] == 1:
-        crash_map[r][c] = 0
-        return
-    else:
-        a = crash_map[r][c] - 1
-        for u in range(r-a, r+a+1):
-            if crash_map[u][c] > 1:
-                crash(u, c, crash_map)
-                crash_map[u][c] = 0 
-            else:
-                crash_map[u][c] = 0 
-        for w in range(c-a, c+a+1):
-            if crash_map[r][w] > 1:
-                crash(r, w, crash_map)
-                crash_map[r][w] = 0
-            else:
-                crash_map[r][w] = 0
+        else:
+            Q.append([r, c])
+            while Q:
+                a, b = Q.popleft()
+                for x in range(a-block_map[a][b]+1, a + block_map[a][b]):
+                    if 0 <= x < H:
+                        if block_map[x][b] != 0:
+                            if block_map[x][b] == 1:
+                                block_map[x][b] = 0
+                            elif block_map[x][b] > 1:
+                                Q.append([x, b])
+                for y in range(b-block_map[a][b]+1, b + block_map[a][b]):
+                    if 0 <= y < W:
+                        if block_map[a][y] != 0:
+                            if block_map[a][y] == 1:
+                                block_map[a][y] = 0
+                            elif block_map[a][y] > 1:
+                                Q.append([a, y])
+            for k in range(H-1 , -1, -1):
+                for d in range(W-1, -1, -1):
+                    if block_map[k][d] == 0:
+                        for g in range(k, -1, -1):
+                            if block_map[g][d] != 0:
+                                block_map[k][d], block_map[g][d] = block_map[g][d], block_map[k][d]
+            pprint(block_map)
+            
 
 
 N = int(input())
 
 for tc in range(1, N+1):
-    result = 1e9
-    N, W, H = map(int, input().split())
+    Q = deque()
+    times, W, H = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(H)]
-    for i in range(H):
-        for j in range(W):
-            if board[i][j] != 0 and (i == 0 or board[i-1][j] == 0):
-                DFS(i, j, board, 1)
+    a = copy.deepcopy(board)
+    top_list = [-1 for _ in range(W)]
+    print(top_list)
+    for i in range(W):
+        for j in range(H):
+            if board[j][i]:
+                top_list[i] = j
+                break
+    print(top_list)
+    for k in range(W):
+        if top_list[k] != -1:
+            DFS(top_list[k], k, a, 0)
+
--- 191114/prison.py
+++ 191114/prison.py
@@ -1,82 +1,41 @@
 import sys
 sys.stdin = open('prison.txt', 'r')
 from pprint import pprint
-def DFS(r, c, t):
-    global result
-    if t == T:
-        return
-    if board[r][c] == 1:
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
-        for h in range(4):
-            x = r + dx[h]
-            y = c + dy[h]
+from collections import deque
+
+IN = [[1, 2, 5, 6], [1, 2, 4, 7], [1, 3, 4, 5], [1, 3, 6, 7]]
+OUT = [[1, 2, 4, 7], [1, 2, 5, 6], [1, 3, 6, 7], [1, 3, 4, 5]]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def BFS(r, c):
+    Q.append([r, c, 1])
+    while Q:
+        a, b, t = Q.popleft()
+        if t == T:
+            return
+        for i in range(4):
+            if board[a][b] not in OUT[i]:
+                continue
+            x = a + dx[i]
+            y = b + dy[i]
             if 0 <= x < H and 0 <= y < W:
-                if board[x][y] != 0:
+                if board[x][y] not in IN[i]:
+                    continue
+                else:
                     if visit[x][y] == 0:
                         visit[x][y] = 1
-                        DFS(x, y, t+1)
-    elif board[r][c] == 2:
-        dy = [-1, 1]
-        for h in range(2):
-            x = r + dy[h]
-            y = c 
-            if 0 <= y < W:
-                if board[x][y] != 0:
-                    if visit[x][y] == 0:
-                        visit[x][y] = 1
-                        DFS(x, y, t+1)
-    elif board[r][c] == 3:
-        dx = [-1, 1]
-        for h in range(2):
-            x = r 
-            y = c + dx[h]
-            if 0 <= y < W:
-                if board[x][y] != 0:
-                    if visit[x][y] == 0:
-                        visit[x][y] = 1
-                        DFS(x, y, t+1)
-    elif board[r][c] == 4:
-        if c+1 < W:
-            if board[r][c+1] != 0:
-                if visit[r][c+1] == 0:
-                    visit[r][c+1] = 1
-                    DFS(r, c+1, t+1)
-    elif board[r][c] == 5:
-        if 0 <= c-1:
-            if board[r][c-1] != 0:
-                if visit[r][c-1] == 0:
-                    visit[r][c-1] = 1
-                    DFS(r, c-1, t+1)
-    elif board[r][c] == 6:
-        if r + 1 < H:
-            if board[r+1][c] != 0:
-                if visit[r+1][c] == 0:
-                    visit[r+1][c] = 1
-                    DFS(r+1, c, t+1)
-    elif board[r][c] == 7:
-        if 0 <= r - 1:
-            if board[r-1][c] != 0:
-                if visit[r-1][c] == 0:
-                    visit[r-1][c] = 1
-                    DFS(r-1, c, t+1)
-   
-
-
+                        Q.append([x, y, t+1])
 
 N = int(input())
 for tc in range(1, N+1):
-    result = 0 
-    H, W, M_H, M_W, T = map(int, input().split())
+    Q = deque()
+    result = 0
+    H, W, m_r, m_c, T = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(H)]
-    visit = [[0] * W for _ in range(H)]
-    visit[M_H][M_W] = 1
-    DFS(M_H, M_W, 1)
-    for i in range(H):
-        for j in range(W):
-            if visit[i][j] == 1:
-                result += 1
-    print(result)
+    visit = [[0]*W for _ in range(H)]
+    visit[m_r][m_c] = 1
+    BFS(m_r, m_c)
+    for k in range(H):
+        result += visit[k].count(1)
     print('#{} {}'.format(tc, result))
-    for y in range(H):
-        print(visit[y])
