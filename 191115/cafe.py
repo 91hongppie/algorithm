08c64dc603b067c8b0275e56876da0f6892ba2b7
조규홍 <marioz9@naver.com>
2019-11-15 17:56:51 +0900

    191115 first

--- 191115/cafe.py
+++ 191115/cafe.py
@@ -2,30 +2,40 @@ import sys
 sys.stdin = open('cafe.txt', 'r')
 from pprint import pprint
 
-def DFS(r, c, nums, ca_list):
+def DFS(r, c, k, nums, ca_list):
     global result
-    if abs(r-i) == 1 and abs(c-j) == 1:
-        if nums > 2:
-            result = max(result, nums)
-            print(ca_list)
-            pprint(visit)
+    if r == a+1  and c == b-1 and len(ca_list) > 2 :
+        result = max(result, len(ca_list))
+        return
+    if r == a and c == b and nums == 0:
+        x = r + 1
+        y = c + 1
+        if board[x][y] not in ca_list:
+            ca_list.append(board[x][y])
+            DFS(x, y, 3, nums+1, ca_list)
+            ca_list.pop()
+        else:
             return
-    for k in range(4):
-        x = r + dx[k]
-        y = c + dy[k]
+    for u in k_list[k]:
+        x = r + dx[u]
+        y = c + dy[u]
         if 0 <= x < rc and 0 <= y < rc:
-            if board[x][y] in ca_list:
-                continue
-            if visit[x][y] == False:
-                visit[x][y] = True
+            if board[x][y] not in ca_list:
+                if nums == 0 and u ==0:
+                    continue
+                if u == 3:
+                    nums += 1
+                elif u == 0:
+                    nums -= 1
                 ca_list.append(board[x][y])
-                DFS(x, y, nums+1, ca_list)
+                DFS(x, y, u, nums, ca_list)
                 ca_list.pop()
-                visit[x][y] = False
+ 
+    
 
-
-dx = [-1, 1, -1, 1]
-dy = [-1, 1, 1, -1]
+k_list = [[0, 1], [1], [2, 0], [3, 2]]
+dx = [-1, -1, 1, 1]
+dy = [-1, 1, -1, 1] # 0:좌상, 1:우상, 2:좌하, 3:우하
 N = int(input())
 
 for tc in range(1, N+1):
@@ -34,17 +44,13 @@ for tc in range(1, N+1):
     visit = [[False]*rc for _ in range(rc)]
     result = 0
     cafe_list = []
-    for i in range(rc):
-        for j in range(i, rc):
-            if i == 0:
-                if j == 0 or j == rc-1:
-                    continue
-            elif i == rc-1 and j == rc-1:
-                continue
-            if visit[i][j] == False:
-                visit[i][j] = True
+    for i in range(rc-2):
+        for j in range(1, rc-1):
                 cafe_list.append(board[i][j])
-                DFS(i, j, 1, cafe_list)
+                dir_list = [0, 0, 0, 0]
+                a, b = i, j
+                DFS(i, j, 3, 0, cafe_list)
                 cafe_list.pop()
-                visit[i][j] = False
-    print(result)
+    if result == 0:
+        result = -1
+    print('#{} {}'.format(tc, result))
--- 191115/cell.py
+++ 191115/cell.py
@@ -1,7 +1,41 @@
 import sys
 sys.stdin = open('cell.txt', 'r')
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 N = int(input())
-
 for tc in range(1, N+1):
-    
+    r, c, times = map(int, input().split())
+    cells = [list(map(int, input().split())) for _ in range(r)]
+    cells_dict = {}
+    cells_list = []
+    for i in range(r):
+        for j in range(c):
+            if cells[i][j] > 0:
+                cells_dict[(i, j)] = (cells[i][j], cells[i][j])
+                cells_list.append([i, j])
+    i = 1
+    while i < times:
+        new_cells = {}
+        for w, u in cells_dict.items():
+            if i == u[1]:
+                for h in range(4):
+                    x = w[0] + dx[h]
+                    y = w[1] + dy[h]
+                    if [x, y] in cells_list:
+                        continue
+                    cells_list.append([x, y])
+                    if (x, y) not in cells_dict:
+                        new_cells[(x, y)] = (u[0], u[0]+u[1]+1)
+                    else:
+                        if u[0]+u[1]+1 > new_cells[(x, y)]:
+                            new_cells[(x, y)] = (u[0], u[0]+u[1]+1)
+        for p, s in new_cells.items():
+            cells_dict[(p[0], p[1])] = s
+        i += 1
+    result = 0
+    for u in cells_dict.values():
+        if u[1] >= times:
+            result += 1
+    print(result)
+            
